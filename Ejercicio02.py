96ffcc7d2eaff472b634f0557f7a94374c67cc79
Edgard Cuellar <icedgard19701@outlook.es>
2019-06-25 08:36:34 -0500

    Ejercicio 2

---
+++ Ejercicio02.py
@@ -0,0 +1,7 @@
+#La historia de usuario es una tecnica usada para poder, de manera sencilla, resumir y tener en claro
+#los principales puntos a tratar al momento de trabajar un proyecto, debe incluir un breve contexto, el nombre
+#del solicitante, los requerimientos y al final el "grado de dificultad" del trabajo
+
+#Tienda de Raul:
+#Necesita llevar el registro de las cosas que entran y salen de su almacen
+#3
